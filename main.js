1ed4cfc2ebb352e139a6144ea37e7d970ef9e95f
Talisson Oliveira <talissonlux@outlook.com>
2022-05-31 19:25:34 -0300

    feat: change the active menu when scrolling the page

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopHasReachedOrPassedTargetLine = targetLine >= sectionTop
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndHasPassedTargetLine = sectionEndsAt <= targetLine
+  const sectionBoundaries = sectionTopHasReachedOrPassedTargetLine && !sectionEndHasPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
@@ -12,8 +37,6 @@ function showNavOnScroll() {
   } else {
     navigation.classList.remove('scroll')
   }
-
-  // window.scrollTo(0, document.body.scrollHeight)
 }
 
 function showBackToTopButtonOnScroll() {
@@ -46,5 +69,8 @@ ScrollReveal({
   #services .card,
   #about,
   #about header,
-  #about .content
+  #about .content,
+  #contact,
+  #contact header,
+  #contact .content
 `)
